2c6698767be04d48773782c13d20235417ad3b26
Dhruv Pancholi <160162577+dhruv457457@users.noreply.github.com>
2025-05-17 12:46:23 +0530

    fix

--- nullkey-frontend/src/utils/contr.js
+++ nullkey-frontend/src/utils/contr.js
@@ -1,4 +1,4 @@
-import abi from "./abi.json"; // make sure this file is placed here
+import abi from "./ab.json"; // make sure this file is placed here
 
 export const CONTRACT_ADDRESS = '0x07538f0e5549eb9a8626fa77a018d7691c26c9600d28fb7fb273abf7ffceabcc';
 export const RPC_URL = 'https://starknet-sepolia.public.blastapi.io/rpc/v0_8'; // or other RPC
